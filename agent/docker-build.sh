a0f32fbf6126bf4b1533aa4ca90d394aa7b8bf50
Leena Unnikrishnan <leunnikr@htc1.carbonate.uits.iu.edu>
2018-04-27 17:36:14 -0400

    Versioning etc. for the agent docker images

--- agent/docker-build.sh
+++ agent/docker-build.sh
@@ -13,5 +13,22 @@ VERSION=$1
 TAG=$2
 
 echo "Building docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG ..."
-
 docker build -t docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG --build-arg VERSION=$VERSION .
+
+if [ $? -eq 0 ]; then
+  MYDATE=$(date +%Y%m%d)
+  # create a target image that refers to the above image, and has the tag 
+  # "version-yyyymmdd"
+  docker tag docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG \
+    docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$VERSION-$MYDATE
+
+  # push both images to the HTRC docker registry
+  echo "Pushing $HTRC_SERVICE:$TAG, $HTRC_SERVICE:$VERSION-$MYDATE into docker-registry.htrc.indiana.edu ..."
+  docker push docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG
+  docker push docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$VERSION-$MYDATE
+
+  # remove the docker image name with tag "version-yyyymmdd" from the local 
+  # environment 
+  # docker rmi docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$VERSION-$MYDATE
+fi
+
