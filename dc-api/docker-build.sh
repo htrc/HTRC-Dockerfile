af631d9599c64d908fe62db1808082e5a45771cc
Samitha Liyanage <samitha.liyanage@gmail.com>
2018-10-22 14:05:49 -0400

    changed dcapi Dockerfile and docker-build script.

--- dc-api/docker-build.sh
+++ dc-api/docker-build.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-version=$(../git-describe-remote.awk https://github.com/htrc/HTRC-DataCapsules.git)
-echo " Version $version"
-date=$(date +%Y%m%d)
-docker build  --no-cache -t docker-registry.htrc.indiana.edu/dc-api:$version-$date  .
-docker push docker-registry.htrc.indiana.edu/dc-api:$version-$date
-docker tag docker-registry.htrc.indiana.edu/dc-api:$version-$date docker-registry.htrc.indiana.edu/dc-api:latest
-docker push docker-registry.htrc.indiana.edu/dc-api:latest
+
+if [ "$#" -ne 2 ]; then
+  echo "Unexpected no. of arguments."
+  echo "Usage: bash docker-build.sh warVersion dockerImageTag"
+  echo "Examples: bash docker-build.sh 3.2.8-SNAPSHOT dev"
+  echo "          bash docker-build.sh 3.2.8 prod"
+  exit 1
+fi
+
+HTRC_SERVICE="dc-api"
+VERSION=$1
+TAG=$2
+
+echo "Building docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG ..."
+docker build -t docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG --build-arg VERSION=$VERSION .
+
+if [ $? -eq 0 ]; then
+  MYDATE=$(date +%Y%m%d)
+  # create a target image that refers to the above image, and has the tag
+  # "version-yyyymmdd"
+  docker tag docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG \
+    docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$VERSION-$MYDATE
+
+  # push both images to the HTRC docker registry
+  echo "Pushing $HTRC_SERVICE:$TAG, $HTRC_SERVICE:$VERSION-$MYDATE into docker-registry.htrc.indiana.edu ..."
+  docker push docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$TAG
+  docker push docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$VERSION-$MYDATE
+
+  # remove the docker image name with tag "version-yyyymmdd" from the local
+  # environment
+  # docker rmi docker-registry.htrc.indiana.edu/$HTRC_SERVICE:$VERSION-$MYDATE
+fi
+
 
