2f70a6b34e50b43619213adec40628c70b0781aa
Samitha Liyanage <shliyana@Samithas-MBP.attlocal.net>
2020-04-13 22:17:52 -0400

    Edited wso2is Dockerfile and build script.

--- wso2is/docker-build.sh
+++ wso2is/docker-build.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker build -t docker-registry.htrc.indiana.edu/wso2is-1.8.0 .
+
+if [ "$#" -ne 2 ]; then
+  echo "Unexpected no. of arguments."
+  echo "Usage: bash docker-build.sh warVersion dockerImageTag"
+  echo "Examples: bash docker-build.sh 3.2.8-SNAPSHOT dev"
+  echo "          bash docker-build.sh 3.2.8 prod"
+  exit 1
+fi
+
+VERSION=$1
+TAG=$2
+docker build -t docker-registry.htrc.indiana.edu/wso2is:$TAG --build-arg VERSION=$VERSION .
